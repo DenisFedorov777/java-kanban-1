80a3df196f59e0aaf39ca414b2dbd294073f495d
gr1spy <george.pozdnyakov@gmail.com>
2022-08-14 14:48:59 +0300

    Переименован SIZE_MAX_HISTORY, обновлен README.md

--- src/main/managers/InMemoryHistoryManager.java
+++ src/main/managers/InMemoryHistoryManager.java
@@ -6,12 +6,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    public static final Integer HISTORY_MAX_SIZE = 10;
+    public static final Integer SIZE_MAX_HISTORY = 10;
     private static final List<Task> historyList = new LinkedList<>();
 
     @Override
     public void add(Task task) {
-        if (historyList.size() < HISTORY_MAX_SIZE) {
+        if (historyList.size() < SIZE_MAX_HISTORY) {
             historyList.add(task);
         } else {
             historyList.remove(0);
